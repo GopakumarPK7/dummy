39869de1035801ef03aa0905c0e228fa74bdf79b
GopakumarPK7 <gopakumarpk7@gmail.com>
2022-07-25 11:44:39 +0530

    added

---
+++ src/test/java/MavenDummy/MavenDummy.java
@@ -0,0 +1,24 @@
+package MavenDummy;
+
+import org.testng.annotations.Test;
+
+public class MavenDummy 
+{
+	@Test
+	public void practiceMaven()
+	{
+	System.out.println("TC -");
+	}
+	@Test
+	public void practiceMaven1()
+	{
+	System.out.println("TC - 2");
+
+	}
+	@Test
+	public void practiceMaven2()
+	{
+	System.out.println("TC - 3");
+	System.out.println("TC - 5");
+	}
+}
